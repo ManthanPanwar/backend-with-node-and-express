ec55a40742f31e3aa334180f9fceff541ffa90a2
ManthanPanwar <manthandpr@gmail.com>
2024-07-23 17:12:00 +0530

    first Node program

---
+++ node/1notesOnNode.js
@@ -0,0 +1,19 @@
+/*
+Node.js is a javascript runtime.
+
+You use node.js to write server side code
+
+Node's role in web Development
+[
+  1. run server(create server and listen to incoming requests)
+
+  2. business logic(handle requests, validate input, connect to database)
+
+  3. responses(return responses in form of html, json,...)
+]
+
+alternatives of Node
+[
+  python, ASP.NET, Ruby, php, ...
+]
+*/
